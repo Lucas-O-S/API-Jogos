6741e5cf05dece80b9863233aa99204d3a9475c8
Lucas-O-S <lucasoscomercial@gmail.com>
2024-11-18 12:57:54 -0300

    Controller Pontuação

--- Java/ranking-jogos/src/main/java/com/rankingjogos/ranking_jogos/controllers/PontuacaoController.java
+++ Java/ranking-jogos/src/main/java/com/rankingjogos/ranking_jogos/controllers/PontuacaoController.java
@@ -1,11 +1,41 @@
 package com.rankingjogos.ranking_jogos.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rankingjogos.ranking_jogos.application.PontuacaoService;
+import com.rankingjogos.ranking_jogos.model.Pontuacao;
 
 @RestController
 @RequestMapping("/api/pontucao")
 public class PontuacaoController {
+    private final PontuacaoService pontuacaoService;
 
+    public PontuacaoController(PontuacaoService pontuacaoService){
+        this.pontuacaoService = pontuacaoService; 
+    }
+
+    @PostMapping
+    public ResponseEntity<Pontuacao> CriarPontuacao(@RequestBody Pontuacao pontuacao){
+        return ResponseEntity.ok(pontuacaoService.Save(pontuacao));
+    }
+
+    @RequestMapping("/jogo/{id}")
+    public ResponseEntity<List<Pontuacao>> BuscarPontuacaoJogo(@RequestAttribute UUID id){
+        return ResponseEntity.ok(pontuacaoService.FindByJogoId(id));
+    }
+
+    @RequestMapping("/jogador/{id}")
+    public ResponseEntity<List<Pontuacao>> BuscarPontuacaoJogador(@RequestAttribute UUID id){
+        return ResponseEntity.ok(pontuacaoService.FindByJogadorId(id));
+    }
 
 }
+
