ec50659d732893e6862b054c28365cd9b6368507
Lucas-O-S <lucasoscomercial@gmail.com>
2024-11-22 13:19:43 -0300

    Push inicial de interface

---
+++ CSHTML/InterfaceRankingJogos/InterfaceRankingJogos/Models/Jogador.cs
@@ -0,0 +1,11 @@
+﻿namespace InterfaceRankingJogos.Models
+{
+    public class Jogador
+    {
+        public Guid id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int idade { get; set; }
+
+    }
+}
---
+++ CSHTML/InterfaceRankingJogos/InterfaceRankingJogos/Models/Jogo.cs
@@ -0,0 +1,9 @@
+﻿namespace InterfaceRankingJogos.Models
+{
+    public class Jogo
+    {
+        public Guid id { get; set; }  
+        public string nome { get; set; }
+        public string descricao { get; set; }
+    }
+}
